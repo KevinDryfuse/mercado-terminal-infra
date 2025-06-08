79c55e77433016915cc6ba66f43b6d38b6b3a891
Kevin Dryfuse <kevin@kevindryfuse.com>
2025-06-08 17:45:41 -0400

    Standardizes the structure of the terraform modules

--- modules/network/outputs.tf
+++ modules/network/outputs.tf
@@ -1,7 +1,14 @@
 output "vnet_id" {
-  value = azurerm_virtual_network.vnet.id
+  description = "The ID of the Virtual Network"
+  value       = azurerm_virtual_network.vnet.id
+}
+
+output "apps_subnet_id" {
+  description = "The ID of the application subnet"
+  value       = azurerm_subnet.apps_subnet.id
 }
 
 output "db_subnet_id" {
-  value = azurerm_subnet.db.id
+  description = "The ID of the database subnet"
+  value       = azurerm_subnet.db_subnet.id
 }
--- modules/network/variables.tf
+++ modules/network/variables.tf
@@ -1,6 +1,44 @@
-variable "vnet_name" {}
-variable "address_space" {
-  type = list(string)
+variable "resource_group_name" {
+  type        = string
+  description = "The name of the resource group"
 }
-variable "location" {}
-variable "resource_group_name" {}
+
+variable "location" {
+  type        = string
+  description = "The Azure region to deploy into"
+}
+
+variable "vnet_name" {
+  type        = string
+  description = "The name of the Virtual Network"
+}
+
+variable "address_space" {
+  type        = list(string)
+  default     = ["10.0.0.0/16"]
+  description = "The address space for the virtual network"
+}
+
+variable "apps_subnet_name" {
+  type        = string
+  default     = "apps-subnet"
+  description = "Name for the application subnet"
+}
+
+variable "apps_subnet_prefix" {
+  type        = string
+  default     = "10.0.1.0/24"
+  description = "CIDR prefix for the application subnet"
+}
+
+variable "db_subnet_name" {
+  type        = string
+  default     = "db-subnet"
+  description = "Name for the database subnet"
+}
+
+variable "db_subnet_prefix" {
+  type        = string
+  default     = "10.0.2.0/24"
+  description = "CIDR prefix for the database subnet"
+}

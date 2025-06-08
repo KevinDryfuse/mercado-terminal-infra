fd25037165e1ce9a80d6cc0ec0997995c56b59c2
Kevin Dryfuse <kevin@kevindryfuse.com>
2025-06-08 18:18:31 -0400

    Adds a matrix deploy in preparation for future clients

--- envs/dg/variables.tf
+++ envs/dg/variables.tf
@@ -1,4 +1,11 @@
+variable "db_admin_user" {
+  description = "Database admin username for DG"
+  type        = string
+  sensitive   = true
+}
+
 variable "db_admin_password" {
-  type      = string
-  sensitive = true
+  description = "Database admin password for DG"
+  type        = string
+  sensitive   = true
 }

699595c38114f76bd55b3952a87d6ddde6604b54
Kevin Dryfuse <kevin@kevindryfuse.com>
2025-06-09 06:29:07 -0400

    Adds terraform scripts to create vnets and subnets

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -1,3 +1,4 @@
+# Resource Group
 resource "azurerm_resource_group" "main" {
   name     = "rg-terminal-${var.client_name}-${var.environment}"
   location = var.location
@@ -8,4 +9,35 @@ resource "azurerm_resource_group" "main" {
     Environment = var.environment
     Service     = "network"
   }
+}
+
+# VNet
+resource "azurerm_virtual_network" "main" {
+  name                = "vnet-terminal-${var.client_name}-${var.environment}"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  address_space       = ["10.0.0.0/16"]
+
+  tags = {
+    Product     = "terminal"
+    Client      = var.client_name
+    Environment = var.environment
+    Service     = "network"
+  }
+}
+
+# App Subnet
+resource "azurerm_subnet" "app" {
+  name                 = "snet-app-${var.client_name}-${var.environment}"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# DB Subnet
+resource "azurerm_subnet" "db" {
+  name                 = "snet-db-${var.client_name}-${var.environment}"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.2.0/24"]
 }
--- modules/network/outputs.tf
+++ modules/network/outputs.tf
@@ -1,3 +1,15 @@
 output "resource_group_id" {
   value = azurerm_resource_group.main.id
 }
+
+output "virtual_network_id" {
+  value = azurerm_virtual_network.main.id
+}
+
+output "app_subnet_id" {
+  value = azurerm_subnet.app.id
+}
+
+output "db_subnet_id" {
+  value = azurerm_subnet.db.id
+}
